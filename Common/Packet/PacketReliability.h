af8dd9bc3e601a38f46838fb4a548a009c51006e
JungChan-Sung <97433018+JungChan-Sung@users.noreply.github.com>
2026-06-08 05:34:47 +0900

    PacketReliability 추가

---
+++ Common/Packet/PacketReliability.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <Common/Packet/PacketType.h>
+
+namespace common::packet
+{
+	enum class PacketReliability
+	{
+		Unreliable,
+		Reliable,
+	};
+
+	[[nodiscard]] inline PacketReliability GetPacketReliability(PacketType packetType) noexcept
+	{
+		switch (packetType)
+		{
+		case PacketType::JoinRoomRequest:
+		case PacketType::JoinRoomResponse:
+		case PacketType::LeaveRequest:
+			return PacketReliability::Reliable;
+
+		default:
+			return PacketReliability::Unreliable;
+		}
+	}
+
+	[[nodiscard]] inline bool IsReliablePacketType(PacketType packetType) noexcept
+	{
+		return GetPacketReliability(packetType) == PacketReliability::Reliable;
+	}
+}

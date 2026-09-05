5c9207aaab537714ff9c58ab2485439c351a78e5
JungChan-Sung <97433018+JungChan-Sung@users.noreply.github.com>
2026-09-05 23:37:58 +0900

    Common/ Identity, String, Time 주석 추가

--- Common/Identity/IdentityTypes.h
+++ Common/Identity/IdentityTypes.h
@@ -4,6 +4,7 @@
 
 namespace common::identity
 {
+	// DB에 저장되어 세션 수명을 넘어 유지되는 영속성 식별자 타입을 정의
 	using AccountId = std::int64_t;
 	using PersistentPlayerId = std::int64_t;
 	using MatchId = std::int64_t;
--- Common/Time/TimeTypes.h
+++ Common/Time/TimeTypes.h
@@ -4,11 +4,13 @@
 
 namespace common::time
 {
+	// Timeout과 경과 시간 계산에는 시스템 시각 변경의 영향을 받지 않는 Steady Clock을 사용
 	using SteadyClock = std::chrono::steady_clock;
 	using Clock = SteadyClock;
 	using TimePoint = Clock::time_point;
 	using Duration = Clock::duration;
 
+	// 로그나 영속성 데이터처럼 실제 시각이 필요한 경우에는 System Clock을 사용
 	using SystemClock = std::chrono::system_clock;
 	using SystemTimePoint = SystemClock::time_point;
 	using SystemDuration = SystemClock::duration;
